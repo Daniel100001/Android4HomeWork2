faf666340d78e0fc9cb1ba62a0ae3a9569e818de
Daniel100001 <danldplayer2gmail.com>
2023-06-18 18:35:52 +0600

    it's Android 4 Home Work 2

--- app/src/main/java/com/example/android4homework1/data/repositories/pagingsources/MangaPagingSource.kt
+++ app/src/main/java/com/example/android4homework1/data/repositories/pagingsources/MangaPagingSource.kt
@@ -1,43 +1,25 @@
 package com.example.android4homework1.data.repositories.pagingsources
 
 import android.net.Uri
-import androidx.paging.PagingSource
-import androidx.paging.PagingState
+import com.example.android4homework1.base.BasePagingSource
 import com.example.android4homework1.data.remote.apiservices.MangaApiService
 import com.example.android4homework1.models.rickAndMortyCharacter.MangaModel
-import retrofit2.HttpException
-import java.io.IOException
 
 private const val CHARACTER_STARTING_PAGE_INDEX = 1
 
-class MangaPagingSource(private var service: MangaApiService) : PagingSource<
-        Int, MangaModel>() {
+class MangaPagingSource(private var service: MangaApiService) :
+    BasePagingSource<MangaModel>() {
 
-    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, MangaModel> {
+    override suspend fun fetchData(params: LoadParams<Int>): LoadResult<Int, MangaModel> {
         val position = params.key ?: CHARACTER_STARTING_PAGE_INDEX
         val pageSize = params.loadSize
-        return try {
-            val response = service.fetchCharacters(pageSize, position)
-            val next = response.links.next
-            val nextPageNumber =
-                Uri.parse(next).getQueryParameter("page[offset]")!!.toInt()
-            LoadResult.Page(
-                data = response.data,
-                prevKey = null,
-                nextKey = nextPageNumber
-            )
-
-        } catch (exception: IOException) {
-            return LoadResult.Error(exception)
-        } catch (exception: HttpException) {
-            return LoadResult.Error(exception)
-        }
-    }
-
-    override fun getRefreshKey(state: PagingState<Int, MangaModel>): Int? {
-        return state.anchorPosition?.let { anchorPosition ->
-            val anchorPage = state.closestPageToPosition(anchorPosition)
-            anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
-        }
+        val response = service.fetchCharacters(pageSize, position)
+        val next = response.links.next
+        val nextPageNumber = Uri.parse(next).getQueryParameter("page[offset]")!!.toInt()
+        return LoadResult.Page(
+            data = response.data,
+            prevKey = null,
+            nextKey = nextPageNumber
+        )
     }
 }
