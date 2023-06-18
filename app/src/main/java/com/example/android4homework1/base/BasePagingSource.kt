667b4635a52c2e92f20d11897af4163bb06da999
Daniel100001 <danldplayer2gmail.com>
2023-06-18 18:36:06 +0600

    it's Android 4 Home Work 2

--- app/src/main/java/com/example/android4homework1/base/BasePagingSource.kt
+++ app/src/main/java/com/example/android4homework1/base/BasePagingSource.kt
@@ -1,40 +1,28 @@
-//package com.example.android4homework1.base
-//
-//import android.net.Uri
-//import androidx.paging.PagingSource
-//import androidx.paging.PagingState
-//import com.example.android4homework1.data.remote.apiservices.MangaApiService
-//import com.example.android4homework1.models.rickAndMortyCharacter.MangaModel
-//import retrofit2.HttpException
-//import java.io.IOException
-//
-//private const val CHARACTER_STARTING_PAGE_INDEX = 1
-//
-//abstract class BasePagingSource(private var service: ) : PagingSource<Int,MangaModel>(){
-//
-//     suspend fun loadBase(params: LoadParams<Int>): LoadResult<Int, MangaModel> {
-//        val position = params.key ?: CHARACTER_STARTING_PAGE_INDEX
-//        return try {
-//            val response = service.fetchCharacters(params.loadSize,position)
-//            val next = response.links.next
-//            val nextPageNumber =
-//                Uri.parse(next).getQueryParameter("page[offset]")!!.toInt()
-//            LoadResult.Page(
-//                data = response.data,
-//                prevKey = null,
-//                nextKey = nextPageNumber
-//            )
-//        } catch (exception: IOException) {
-//            return LoadResult.Error(exception)
-//        } catch (exception: HttpException) {
-//            return LoadResult.Error(exception)
-//        }
-//    }
-//
-//     fun getRefreshKeyBase(state: PagingState<Int, MangaModel>): Int? {
-//        return state.anchorPosition?.let { anchorPosition ->
-//            val anchorPage = state.closestPageToPosition(anchorPosition)
-//            anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
-//        }
-//    }
-//}
+package com.example.android4homework1.base
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import retrofit2.HttpException
+import java.io.IOException
+
+abstract class BasePagingSource<Value : Any> : PagingSource<Int, Value>() {
+
+    abstract suspend fun fetchData(params: LoadParams<Int>): LoadResult<Int, Value>
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Value> {
+        return try {
+            fetchData(params)
+        } catch (exception: IOException) {
+            LoadResult.Error(exception)
+        } catch (exception: HttpException) {
+            LoadResult.Error(exception)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, Value>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            val anchorPage = state.closestPageToPosition(anchorPosition)
+            anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
+        }
+    }
+}
